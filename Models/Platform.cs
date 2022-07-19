133ddc7a0e0ed3964e602f583155beb8fe9573b9
Flavio Coutinho <coutinhoflavio20@gmail.com>
2022-07-18 21:48:02 -0300

    initial commit

---
+++ Models/Platform.cs
@@ -0,0 +1,13 @@
+namespace PlatformService.Models
+{
+    public class Plataform
+    {
+        public int Id { get; set; }
+
+        public string name { get; set; }
+
+        public string publisher { get; set; }
+
+        public string cost { get; set; }
+    }
+}
