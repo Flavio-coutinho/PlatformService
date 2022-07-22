f0d4a2d8172344ccc3edaa3b2b0e09316cfda806
Flavio Coutinho <coutinhoflavio20@gmail.com>
2022-07-22 00:24:41 -0300

    1/10

--- Models/Platform.cs
+++ Models/Platform.cs
@@ -1,13 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PlatformService.Models
 {
     public class Plataform
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
 
+        [Required]
         public string name { get; set; }
 
+        [Required]
         public string publisher { get; set; }
 
+        [Required]
         public string cost { get; set; }
     }
 }
